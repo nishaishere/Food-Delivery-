2405ec18659c146266951e89498c567f0533f547
Nisha Bhaskar <bhaskarnisha840@gmail.com>
2026-03-09 23:42:44 -0700

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,121 @@
+// ================= ICONS =================
+import logo from './logo.png'
+import add_icon from './add_icon.png'
+import add_icon_green from './add_icon_green.png'
+import add_icon_white from './add_icon_white.png'
+import order_icon from './order_icon.png'
+import parcel_icon from './parcel_icon.png'
+import profile_icon from './profile_icon.png'
+import profile_image from './profile_image.png'
+import bag_icon from './bag_icon.png'
+import basket_icon from './basket_icon.png'
+import cross_icon from './cross_icon.png'
+import search_icon from './search_icon.png'
+import remove_icon_red from './remove_icon_red.png'
+import rating_stars from './rating_starts.png' // renamed variable only
+import upload_area from './upload_area.png'
+import logout_icon from './logout_icon.png'
+import facebook_icon from './facebook_icon.png'
+import linkedin_icon from './linkedin_icon.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import header_img from './header_img.png'
+
+// ================= MENU IMAGES =================
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+// ================= FOOD IMAGES =================
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+
+// ================= ASSETS EXPORT =================
+export const assets = {
+  logo,
+  add_icon,
+  add_icon_green,
+  add_icon_white,
+  order_icon,
+  parcel_icon,
+  profile_icon,
+  profile_image,
+  bag_icon,
+  basket_icon,
+  cross_icon,
+  search_icon,
+  remove_icon_red,
+  rating_stars,
+  upload_area,
+  logout_icon,
+  facebook_icon,
+  linkedin_icon,
+  app_store,
+  play_store,
+  header_img,
+}
+
+// ================= MENU LIST =================
+export const menu_list = [
+  { menu_name: "Salad", menu_image: menu_1 },
+  { menu_name: "Rolls", menu_image: menu_2 },
+  { menu_name: "Desserts", menu_image: menu_3 },
+  { menu_name: "Sandwich", menu_image: menu_4 },
+  { menu_name: "Cake", menu_image: menu_5 },
+  { menu_name: "Pure Veg", menu_image: menu_6 },
+  { menu_name: "Pasta", menu_image: menu_7 },
+  { menu_name: "Noodles", menu_image: menu_8 },
+]
+
+// ================= FOOD LIST =================
+export const food_list = [
+  { _id: "1", name: "Greek Salad", image: food_1, price: 120, category: "Salad" },
+  { _id: "2", name: "Veg Roll", image: food_2, price: 80, category: "Rolls" },
+  { _id: "3", name: "Chocolate Cake", image: food_3, price: 250, category: "Cake" },
+  { _id: "4", name: "Veg Sandwich", image: food_4, price: 100, category: "Sandwich" },
+  { _id: "5", name: "White Pasta", image: food_5, price: 180, category: "Pasta" },
+  { _id: "6", name: "Hakka Noodles", image: food_6, price: 150, category: "Noodles" },
+  { _id: "7", name: "Fruit Salad", image: food_7, price: 110, category: "Salad" },
+  { _id: "8", name: "Paneer Roll", image: food_8, price: 90, category: "Rolls" },
+  { _id: "9", name: "Brownie", image: food_9, price: 200, category: "Desserts" },
+  { _id: "10", name: "Red Sauce Pasta", image: food_10, price: 170, category: "Pasta" },
+  { _id: "11", name: "Veg Burger", image: food_11, price: 130, category: "Sandwich" },
+  { _id: "12", name: "Chowmein", image: food_12, price: 140, category: "Noodles" },
+]
+
+// ================= BACKEND URL =================
+export const url = "http://localhost:4000"
